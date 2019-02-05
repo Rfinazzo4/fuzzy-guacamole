4e1958cdd111d4bee2641ad04f1c4fb71be608d9
Parfait Mwamba <parfaitmwamba@gmail.com>
2019-02-05 15:55:50 -0800

    Merging

---
+++ src/Model/CookBook.java
@@ -0,0 +1,28 @@
+package Model;
+
+import java.util.ArrayList;
+public class CookBook {
+	private ArrayList<Recipe> recipes;
+	private int size;
+	
+	public CookBook() {
+		//TODO code
+	}
+	
+	public void addRecipe() {
+		
+	}
+	
+	public void printRecipes() {
+		
+	}
+	
+	public void findRecipe(String name) {
+		
+	}
+	
+	public void changeRecipe(String name) {
+		
+	}
+
+}
--- src/Model/Recipe.java
+++ src/Model/Recipe.java
@@ -1,10 +1,15 @@
+package Model;
 import java.util.ArrayList;
 public class Recipe {
 	private String name;
 	private ArrayList<Ingredient> ingredients;
+	private int servings;
 	
-	public Recipe(String n) {
-		name = n;
+	public Recipe() {
+		
+	}
+	
+	public Recipe(String filename) {
 		ingredients = new ArrayList<>();
 	}
 
@@ -15,4 +20,22 @@ public class Recipe {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public int getServings() {
+		return servings;
+	}
+
+	public void setServings(int servings) {
+		this.servings = servings;
+	}
+	
+	public void addItem(String name, double quantity, String type) {
+		
+	}
+	
+	public void removeItem(String name) {
+		
+	}
+	
+	
 }
