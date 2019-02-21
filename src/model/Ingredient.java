c1b9e65dbe50fb08e4abd253511c0c01778b35f2
Parfait Mwamba <parfaitmwamba@gmail.com>
2019-02-21 13:02:27 -0800

    Included Ingredient in Master

--- src/model/Ingredient.java
+++ src/model/Ingredient.java
@@ -1,5 +1,40 @@
 package model;
 
 public class Ingredient {
+	private String name;
+	private String measurement;
+	double quantity;
+	
+	Ingredient(String name, String measurement, double quantity){
+		this.name = name;
+		this.measurement = measurement;
+		this.quantity = quantity;
+	}
+	
+	public String getMeasurement() {
+		return measurement;
+	}
+	public void setMeasurement(String measurement) {
+		this.measurement = measurement;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
+	
+	public String toString() {
+		if(measurement != null || !measurement.equals(""))
+			return quantity + " " + measurement + " of " + name;
+		return quantity + " " + name;
+	}
+	
 	
 }
