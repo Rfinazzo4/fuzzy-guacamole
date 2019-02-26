24aa1b6e1dd074447721e1a494f0e197b52f517b
Rfinazzo4 <finazzo.ryan4@gmail.com>
2019-02-25 21:53:26 -0800

    Added majority of Cookbook and needed constructor in Recipe

--- src/model/Recipe.java
+++ src/model/Recipe.java
@@ -1,5 +1,12 @@
 package model;
+
+import java.util.List;
+
 public class Recipe {
+
+	public Recipe(String name, List<Ingredient> ingred, int serving) {
+		// TODO Auto-generated constructor stub
+	}
 	
 	
 }
