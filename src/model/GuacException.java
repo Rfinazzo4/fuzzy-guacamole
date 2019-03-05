5257a57ed651ef0a3d0c8eee1d02a3c85b1fa87a
Parfait Mwamba <parfaitmwamba@gmail.com>
2019-03-05 10:32:41 -0800

    Added helper classes

---
+++ src/model/GuacException.java
@@ -0,0 +1,7 @@
+package model;
+
+public interface GuacException {
+	public final static String INVALID_SYNTAX = "Invalid Syntax";
+	public final static String EXIT_SAVE = "Error while saving current Cook Book!";
+
+}
